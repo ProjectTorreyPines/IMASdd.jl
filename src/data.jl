fb51e635f2922b309375c261303feeb2e1e9c2df
Orso Meneghini <orso82@gmail.com>
2024-12-02 18:32:44 -0500

    add cocos transform on _setproperty!

--- src/data.jl
+++ src/data.jl
@@ -336,11 +336,11 @@ Returns true if the ids field has no data (or expression)
 NOTE: By default it does not include nor evaluate expressions
 """
 function Base.isempty(@nospecialize(ids::IDS), field::Symbol; include_expr::Bool=false, eval_expr::Bool=false)
-    v = getfield(ids, field)
-    if typeof(v) <: IDSvector # filled arrays of structures
-        return isempty(v)
-    elseif typeof(v) <: IDS # filled structures
-        return isempty(v; include_expr, eval_expr)
+    value = getfield(ids, field)
+    if typeof(value) <: IDSvector # filled arrays of structures
+        return isempty(value)
+    elseif typeof(value) <: IDS # filled structures
+        return isempty(value; include_expr, eval_expr)
     elseif eval_expr
         return getproperty(ids, field, missing) === missing
     elseif include_expr
@@ -450,16 +450,16 @@ function _getproperty(@nospecialize(ids::IDS), field::Symbol; to_cocos::Int)
     end
 end
 
-function _setproperty!(@nospecialize(ids::IDS), field::Symbol, v::Union{AbstractRange,StaticArraysCore.SVector,StaticArraysCore.MVector,SubArray})
-    return _setproperty!(ids, field, collect(v))
+function _setproperty!(@nospecialize(ids::IDS), field::Symbol, value::Union{AbstractRange,StaticArraysCore.SVector,StaticArraysCore.MVector,SubArray}; from_cocos::Int=user_cocos)
+    return _setproperty!(ids, field, collect(value); from_cocos)
 end
 
 """
-    _setproperty!(@nospecialize(ids::IDS), field::Symbol, v::Any)
+    _setproperty!(@nospecialize(ids::IDS), field::Symbol, value::Any)
 
 Like setfield! but also add to list of filled fields
 """
-function _setproperty!(@nospecialize(ids::IDS), field::Symbol, v::Any)
+function _setproperty!(@nospecialize(ids::IDS), field::Symbol, value::Any; from_cocos::Int=user_cocos)
     T = eltype(ids)
     if field in private_fields
         error("Use `setfield!(ids, :$field, ...)` instead of _setproperty!(ids, :$field ...)")
@@ -467,31 +467,39 @@ function _setproperty!(@nospecialize(ids::IDS), field::Symbol, v::Any)
 
     # nice error if type is wrong
     tp = fieldtype_typeof(ids, field)
-    if !(typeof(v) <: tp)
+    if !(typeof(value) <: tp)
         if (T === Float64) || !(tp <: T) # purposely force right type when working with Float64 or the field is not of type T
-            error("`$(typeof(v))` is the wrong type for `$(ulocation(ids, field))`, it should be `$(tp)`")
+            error("`$(typeof(value))` is the wrong type for `$(ulocation(ids, field))`, it should be `$(tp)`")
         else
             try
-                v = convert(tp, v)
+                value = convert(tp, value)
             catch
-                error("Failed to convert `$(typeof(v))` to `$(tp)` for `$(ulocation(ids, field))`")
+                error("Failed to convert `$(typeof(value))` to `$(tp)` for `$(ulocation(ids, field))`")
             end
         end
     end
 
     # set the _parent property of the value being set
-    if typeof(v) <: Union{IDS,IDSvector}
-        setfield!(v, :_parent, WeakRef(ids))
+    if typeof(value) <: Union{IDS,IDSvector}
+        setfield!(value, :_parent, WeakRef(ids))
+    end
+
+    # may need cocos conversion    
+    if (from_cocos != internal_cocos) && (eltype(value) <: Real)
+        cocos_multiplier = transform_cocos_coming_in(ids, field, from_cocos)
+        if cocos_multiplier != 1.0
+            value = cocos_multiplier .* value
+        end
     end
 
     # setfield
-    tmp = setfield!(ids, field, v)
+    tmp = setfield!(ids, field, value)
 
     # add to list of filled fields
     add_filled(ids, field)
 
     # log write access
-    if access_log.enabled && !(typeof(v) <: Union{IDS,IDSvector})
+    if access_log.enabled && !(typeof(value) <: Union{IDS,IDSvector})
         push!(access_log.write, ulocation(ids, field))
     end
 
@@ -553,32 +561,32 @@ function del_filled(@nospecialize(ids::Union{IDS,IDSvector}))
 end
 
 """
-    Base.setproperty!(ids::IDS, field::Symbol, v; skip_non_coordinates::Bool=false, error_on_missing_coordinates::Bool=true)
+    Base.setproperty!(ids::IDS, field::Symbol, value; skip_non_coordinates::Bool=false, error_on_missing_coordinates::Bool=true)
 """
-function Base.setproperty!(@nospecialize(ids::IDS), field::Symbol, v::Any; skip_non_coordinates::Bool=false, error_on_missing_coordinates::Bool=true)
-    return _setproperty!(ids, field, v)
+function Base.setproperty!(@nospecialize(ids::IDS), field::Symbol, value::Any; skip_non_coordinates::Bool=false, error_on_missing_coordinates::Bool=true)
+    return _setproperty!(ids, field, value)
 end
 
 """
-    Base.setproperty!(@nospecialize(ids::IDS), field::Symbol, v::AbstractArray{<:IDS}; skip_non_coordinates::Bool=false, error_on_missing_coordinates::Bool=true)
+    Base.setproperty!(@nospecialize(ids::IDS), field::Symbol, value::AbstractArray{<:IDS}; skip_non_coordinates::Bool=false, error_on_missing_coordinates::Bool=true)
 
 Handle setproperty of entire vectors of IDS structures at once (ids.field is of type IDSvector)
 """
-function Base.setproperty!(@nospecialize(ids::IDS), field::Symbol, v::AbstractArray{<:IDS}; skip_non_coordinates::Bool=false, error_on_missing_coordinates::Bool=true)
+function Base.setproperty!(@nospecialize(ids::IDS), field::Symbol, value::AbstractArray{<:IDS}; skip_non_coordinates::Bool=false, error_on_missing_coordinates::Bool=true)
     orig = getfield(ids, field)
     empty!(orig)
-    append!(orig, v)
+    append!(orig, value)
     add_filled(ids, field)
     return orig
 end
 
 """
-    Base.setproperty!(@nospecialize(ids::IDS), field::Symbol, v::AbstractArray; skip_non_coordinates::Bool=false, error_on_missing_coordinates::Bool=true)
+    Base.setproperty!(@nospecialize(ids::IDS), field::Symbol, value::AbstractArray; skip_non_coordinates::Bool=false, error_on_missing_coordinates::Bool=true)
 
 Ensures coordinates are set before the data that depends on those coordinates.
 If `skip_non_coordinates` is set, then fields that are not coordinates will be silently skipped.
 """
-function Base.setproperty!(@nospecialize(ids::IDS), field::Symbol, v::AbstractArray; skip_non_coordinates::Bool=false, error_on_missing_coordinates::Bool=true)
+function Base.setproperty!(@nospecialize(ids::IDS), field::Symbol, value::AbstractArray; skip_non_coordinates::Bool=false, error_on_missing_coordinates::Bool=true)
     if field ∉ getfield(ids, :_filled) && error_on_missing_coordinates
         # figure out the coordinates
         coords = coordinates(ids, field; to_cocos=internal_cocos)
@@ -593,7 +601,7 @@ function Base.setproperty!(@nospecialize(ids::IDS), field::Symbol, v::AbstractAr
             error("Can't assign data to `$(location(ids, field))` before `$(coords.names)`")
         end
     end
-    return _setproperty!(ids, field, v)
+    return _setproperty!(ids, field, value)
 end
 
 export setproperty!
@@ -685,43 +693,43 @@ function Base.getindex(@nospecialize(ids::IDSvector{T}), i::Int) where {T<:IDSve
     end
 end
 
-function Base.setindex!(@nospecialize(ids::IDSvector{T}), @nospecialize(v::T), i::Integer) where {T<:IDSvectorElement}
-    ids._value[i] = v
-    setfield!(v, :_parent, WeakRef(ids))
+function Base.setindex!(@nospecialize(ids::IDSvector{T}), @nospecialize(value::T), i::Integer) where {T<:IDSvectorElement}
+    ids._value[i] = value
+    setfield!(value, :_parent, WeakRef(ids))
     add_filled(ids)
-    return v
+    return value
 end
 
-function Base.push!(@nospecialize(ids::IDSvector{T}), @nospecialize(v::T)) where {T<:IDSvectorElement}
-    setfield!(v, :_parent, WeakRef(ids))
-    push!(ids._value, v)
+function Base.push!(@nospecialize(ids::IDSvector{T}), @nospecialize(value::T)) where {T<:IDSvectorElement}
+    setfield!(value, :_parent, WeakRef(ids))
+    push!(ids._value, value)
     add_filled(ids)
     return ids
 end
 
-function Base.append!(@nospecialize(ids::IDSvector{T}), @nospecialize(vv::AbstractVector{<:T})) where {T<:IDSvectorElement}
-    for v in vv
-        push!(ids, v)
+function Base.append!(@nospecialize(ids::IDSvector{T}), @nospecialize(values::AbstractVector{<:T})) where {T<:IDSvectorElement}
+    for value in values
+        push!(ids, value)
     end
     return ids
 end
 
-function Base.push!(@nospecialize(ids::IDSvector{T}), @nospecialize(v::Any)) where {T<:IDSvectorElement}
-    return error("`push!` on $(location(ids)) must be of type $(T) and is instead of type $(typeof(v))")
+function Base.push!(@nospecialize(ids::IDSvector{T}), @nospecialize(value::Any)) where {T<:IDSvectorElement}
+    return error("`push!` on $(location(ids)) must be of type $(T) and is instead of type $(typeof(value))")
 end
 
-function Base.pushfirst!(@nospecialize(ids::IDSvector{T}), @nospecialize(v::T)) where {T<:IDSvectorElement}
-    setfield!(v, :_parent, WeakRef(ids))
-    pushfirst!(ids._value, v)
+function Base.pushfirst!(@nospecialize(ids::IDSvector{T}), @nospecialize(value::T)) where {T<:IDSvectorElement}
+    setfield!(value, :_parent, WeakRef(ids))
+    pushfirst!(ids._value, value)
     add_filled(ids)
     return ids
 end
 
-function Base.insert!(@nospecialize(ids::IDSvector{T}), i, @nospecialize(v::T)) where {T<:IDSvectorElement}
-    setfield!(v, :_parent, WeakRef(ids))
-    insert!(ids._value, i, v)
+function Base.insert!(@nospecialize(ids::IDSvector{T}), i, @nospecialize(value::T)) where {T<:IDSvectorElement}
+    setfield!(value, :_parent, WeakRef(ids))
+    insert!(ids._value, i, value)
     add_filled(ids)
-    return v
+    return value
 end
 
 function Base.pop!(@nospecialize(ids::IDSvector{T})) where {T<:IDSvectorElement}
--- test/runtests_cocos.jl
+++ test/runtests_cocos.jl
@@ -7,11 +7,25 @@ include(joinpath(@__DIR__, "test_expressions_dicts.jl"))
 @testset "cocos" begin
 
     eqt1d = IMASdd.equilibrium__time_slice___profiles_1d()
-    eqt1d.psi = [0.1]
-    eqt1d.rho_tor_norm = [1.0]
-    
+
+    # user COCOS same as internal_cocos
+    IMASdd._setproperty!(eqt1d, :psi, [0.1]; from_cocos=11)
+    IMASdd._setproperty!(eqt1d, :rho_tor_norm, [1.0]; from_cocos=11)
+    @test IMASdd.getfield(eqt1d, :psi) == [0.1]
     @test IMASdd._getproperty(eqt1d, :psi; to_cocos=11) == [0.1]
     @test IMASdd._getproperty(eqt1d, :psi; to_cocos=17) == [-0.1]
+    @test IMASdd.getfield(eqt1d, :rho_tor_norm) == [1.0]
     @test IMASdd._getproperty(eqt1d, :rho_tor_norm; to_cocos=11) == [1.0]
     @test IMASdd._getproperty(eqt1d, :rho_tor_norm; to_cocos=17) == [1.0]
+
+    # user COCOS different from internal_cocos
+    IMASdd._setproperty!(eqt1d, :psi, [-0.1]; from_cocos=17)
+    IMASdd._setproperty!(eqt1d, :rho_tor_norm, [1.0]; from_cocos=17)
+    @test IMASdd.getfield(eqt1d, :psi) == [0.1]
+    @test IMASdd._getproperty(eqt1d, :psi; to_cocos=11) == [0.1]
+    @test IMASdd._getproperty(eqt1d, :psi; to_cocos=17) == [-0.1]
+    @test IMASdd.getfield(eqt1d, :rho_tor_norm) == [1.0]
+    @test IMASdd._getproperty(eqt1d, :rho_tor_norm; to_cocos=11) == [1.0]
+    @test IMASdd._getproperty(eqt1d, :rho_tor_norm; to_cocos=17) == [1.0]
+
 end
