7e4e93574bdcf845e301103b91a55b61acc31db3
Min-Gu Yoo <mgyoo86@gmail.com>
2025-02-06 15:18:40 -0800

    Reorganize tests and increase coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,22 @@
-include("runtests_ids.jl")
+# Check if specific test files are requested via ARGS
+if !isempty(ARGS)
+    for testfile in ARGS
+        @info "Running test file: $testfile"
+        include(testfile)
+    end
+else
+    # Default behavior: run all tests
+    include("runtests_ids.jl")
 
-include("runtests_expressions.jl")
+    include("runtests_expressions.jl")
 
-include("runtests_time.jl")
+    include("runtests_time.jl")
 
-include("runtests_itp.jl")
+    include("runtests_itp.jl")
 
-include("runtests_io.jl")
+    include("runtests_io.jl")
 
-include("runtests_findall.jl")
+    include("runtests_io_extended.jl")
+
+    include("runtests_findall.jl")
+end
---
+++ test/runtests_io_extended.jl
@@ -0,0 +1,130 @@
+using IMASdd
+using Test
+import IMASdd.HDF5 as HDF5
+
+@testset "extended HDF IO" begin
+
+    filename = joinpath(dirname(@__DIR__), "sample", "omas_sample_with_attrs.h5")
+    dd = hdf2imas(filename)
+    @testset "hdf_with_attrs" begin
+        filename = joinpath(dirname(@__DIR__), "sample", "omas_sample_with_attrs.h5")
+        fid = HDF5.h5open(filename)
+
+        # Check attributes of the root (dd)
+        attr = HDF5.attrs(fid)
+        @test attr["abstract_type"] == "IDS"
+        @test attr["concrete_type"] == "IMASdd.dd{Float64}"
+        @test attr["description"] == "This is the same omas sample with attributes."
+        @test attr["freeze"] == "true"
+        @test attr["strict"] == "false"
+
+        # Check attributes of time_slice (example of IDSvector)
+        attr = HDF5.attrs(fid["/equilibrium/time_slice"])
+        @test attr["abstract_type"] == "IDSvector"
+        @test attr["concrete_type"] == "IMASdd.IDSvector{IMASdd.equilibrium__time_slice{Float64}}"
+    end
+
+    @testset "hdf2imas with target path" begin
+        # Root IDS (dd)
+        @test dd == hdf2imas(filename, "/")
+        @test dd == hdf2imas(joinpath(dirname(@__DIR__), "sample", "omas_sample.h5"), "/")
+
+        # IDS example
+        eq = hdf2imas(filename, "/equilibrium")
+        @test dd.equilibrium == eq
+
+        # IDSvector example
+        source = hdf2imas(filename, "/core_sources/source")
+        @test dd.core_sources.source == source
+
+        # Dataset examples
+        glob_time = hdf2imas(filename, "/global_time")
+        @test dd.global_time == glob_time
+
+        vaccum_b0 = hdf2imas(filename, "/equilibrium/vacuum_toroidal_field/b0")
+        @test dd.equilibrium.vacuum_toroidal_field.b0 == vaccum_b0
+    end
+
+
+    @testset "h5merge and read_combined_h5" begin
+        tmp_dir1 = mktempdir()
+        tmp_dir2 = mktempdir()
+        tmp_dir3 = mktempdir()
+
+        combined_file_name = joinpath(tmp_dir3, "combined_test.h5")
+
+        imas2hdf(dd, joinpath(tmp_dir1, "11.h5"); strict=false, freeze=false, desc="This is in tmp_dir1")
+        imas2hdf(dd, joinpath(tmp_dir1, "22.h5"); strict=false, freeze=false, desc="This is in tmp_dir1")
+        imas2hdf(dd, joinpath(tmp_dir2, "aa.h5"); strict=false, freeze=false, desc="This is in tmp_dir2")
+        imas2hdf(dd, joinpath(tmp_dir2, "bb.h5"); strict=false, freeze=false, desc="This is in tmp_dir2")
+
+        # Merge files in a single directory
+        h5merge(combined_file_name, tmp_dir1; mode="w", include_base_dir=true)
+        fid = HDF5.h5open(combined_file_name, "r")
+        @test keys(fid) == [basename(tmp_dir1)]
+        @test keys(fid[basename(tmp_dir1)]) == ["11.h5", "22.h5"]
+        close(fid)
+
+        h5merge(combined_file_name, tmp_dir1; mode="w", include_base_dir=false)
+        fid = HDF5.h5open(combined_file_name, "r")
+        @test keys(fid) == ["11.h5", "22.h5"]
+        close(fid)
+
+        # Merge files in multiple directories
+        h5merge(combined_file_name, [tmp_dir1, tmp_dir2]; mode="w", include_base_dir=true)
+        fid = HDF5.h5open(combined_file_name, "r")
+        @test Set(keys(fid)) == Set(basename.([tmp_dir1, tmp_dir2]))
+        @test keys(fid[basename(tmp_dir1)]) == ["11.h5", "22.h5"]
+        @test keys(fid[basename(tmp_dir2)]) == ["aa.h5", "bb.h5"]
+        close(fid)
+
+        h5merge(combined_file_name, [tmp_dir1, tmp_dir2]; mode="w", include_base_dir=false)
+        fid = HDF5.h5open(combined_file_name, "r")
+        @test keys(fid) == ["11.h5", "22.h5", "aa.h5", "bb.h5"]
+        close(fid)
+
+        # Merge including non-hdf5 files
+        open(joinpath(tmp_dir1, "test.txt"), "w") do f
+            return write(f, "This is a test text file.")
+        end
+        h5merge(combined_file_name, [tmp_dir1, tmp_dir2]; mode="w", include_base_dir=false)
+
+        outDict = read_combined_h5(combined_file_name)
+        @test keys(outDict) == Set(["/11.h5", "/22.h5", "/aa.h5", "/bb.h5", "/test.txt"])
+        @test outDict["/test.txt"] == "This is a test text file."
+
+        # pattern test (read only .h5 files)
+        outDict = read_combined_h5(combined_file_name; pattern=r"\.h5$")
+        @test keys(outDict) == Set(["/11.h5", "/22.h5", "/aa.h5", "/bb.h5"])
+
+        # pattern test (read only .txt files)
+        outDict = read_combined_h5(combined_file_name; pattern=r"\.txt$")
+        @test keys(outDict) == Set(["/test.txt"])
+
+        h5merge(combined_file_name, [tmp_dir1, tmp_dir2]; mode="w", include_base_dir=true)
+        outDict = read_combined_h5(combined_file_name)
+        @test outDict[joinpath("/", basename(tmp_dir1), "test.txt")] == "This is a test text file."
+    end
+end
+
+
+
+@testset "Test edge cases" begin
+
+    ori_dd = hdf2imas(joinpath(dirname(@__DIR__), "sample", "omas_sample.h5"))
+    new_dd = hdf2imas(joinpath(dirname(@__DIR__), "sample", "omas_sample_with_attrs.h5"))
+
+    @test new_dd == ori_dd
+
+    tmp_dir = mktempdir()
+
+    imas2hdf(ori_dd, joinpath(tmp_dir, "test.h5"); target_group="/")
+    imas2hdf(ori_dd, joinpath(tmp_dir, "test.h5"); target_group="///")
+    imas2hdf(ori_dd, joinpath(tmp_dir, "test.h5"); target_group="a/b/c")
+    imas2hdf(ori_dd, joinpath(tmp_dir, "test.h5"); target_group="/", mode="a")
+
+    imas2hdf(ori_dd, joinpath(tmp_dir, "test.h5"); target_group="/ori_dd", mode="w")
+    @test_throws ErrorException imas2hdf(ori_dd, joinpath(tmp_dir, "test.h5"); target_group="/ori_dd", mode="a")
+    imas2hdf(ori_dd, joinpath(tmp_dir, "test.h5"); target_group="/ori_dd", mode="a", overwrite=true, verbose=true)
+
+end
