9073d127ddf014fc2fb1dc9470e71462492ea0c6
Orso Meneghini <orso82@gmail.com>
2025-03-03 11:35:37 -0800

    getproperty for IDSvectorGridggdTimeElement
    mention @anchal-physics

--- src/data.jl
+++ src/data.jl
@@ -272,6 +272,25 @@ function Base.getproperty(ids::IDS, field::Symbol; to_cocos::Int=user_cocos)
 end
 
 """
+    Base.getproperty(ids::IDSvectorGridggdTimeElement{T}, field::Symbol) where {T<:Real}
+
+This function links all grid_ggd types with each other
+
+If the grid_ggd has a path defined to another instance of grid_ggd, this instance would automatically return attributed from the referred instance.
+"""
+function Base.getproperty(ids::IDSvectorGridggdTimeElement{T}, field::Symbol) where {T<:Real}
+    if field == :path
+        return getfield(ids, field)
+    else
+        ref_ids_name = Symbol(split(ids.path, "/")[1])
+        grid_ggd_ind = parse(Int64, split(split(ids.path, "(")[2], ")")[1])
+        ref_ids = getfield(top_dd(ids), ref_ids_name)
+        grid_ggd = getfield(ref_ids, :grid_ggd)
+        return getfield(grid_ggd[grid_ggd_ind], field)
+    end
+end
+
+"""
     getproperty(ids::IDS, field::Symbol, default::Any; to_cocos::Int=user_cocos)
 
 Return IDS value for requested field or `default` if field is missing
