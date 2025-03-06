0f2c03b7f3b006f3d5de178755acb561ab6cb43f
Orso Meneghini <orso82@gmail.com>
2025-03-06 09:54:53 -0800

    fix building of documentation

--- docs/make.jl
+++ docs/make.jl
@@ -21,7 +21,8 @@ makedocs(;
     format=Documenter.HTML(;analytics="G-65D8V8C8VQ"),
     sitename="IMASdd",
     checkdocs=:none,
-    pages=["index.md", "api.md", "License" => "license.md", "Notice" => "notice.md"]
+    pages=["index.md", "api.md", "License" => "license.md", "Notice" => "notice.md"],
+    warnonly=true
 )
 
 # Deploy docs
